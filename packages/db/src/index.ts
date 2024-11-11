51503b579b547dfdaf034247266a902d1521f17d
Enguerrand des Vaux <enguerrand@modjo.ai>
2024-11-11 21:53:24 +0100

    minor changes for migration

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,24 +1,23 @@
 import { sql } from "drizzle-orm/sql";
 
 import { publicDb } from "./client";
+import { migrateTenant } from "./migration";
 
 export * from "drizzle-orm/sql";
 export function generateSchemaName(input: string): string {
-  // Remove non-alphanumeric characters and add - instead of
+  // Remove non-alphanumeric characters and add _ instead of
   const sanitizedInput = input
     .replace(/[^a-zA-Z0-9]/g, " ")
     .trim()
     .split(" ")
-    .join("-");
+    .join("_");
   // schema name need to be 60 char max
   return sanitizedInput.slice(0, 60);
 }
 
-export function createTenantSchema(
-  schemaName: string,
-  templateSchemaName = "template_schema",
-) {
-  return publicDb.execute(
-    sql.raw(`CREATE SCHEMA ${schemaName} LIKE ${templateSchemaName};`),
-  );
+export async function createTenantSchema(schemaName: string) {
+  // create schema
+  await publicDb.execute(sql.raw(`CREATE SCHEMA ${schemaName};`));
+  // add all tyhe needed table and structure
+  await migrateTenant(schemaName);
 }
